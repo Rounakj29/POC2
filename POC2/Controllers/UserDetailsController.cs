208c56cdf4a03d2fb31a4aafc81f705d18124568
Rounak Jaiswal <rounakkjaiswal@gmail.com>
2022-03-24 17:23:44 +0530

    Add project files.

---
+++ POC2/Controllers/UserDetailsController.cs
@@ -0,0 +1,64 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using POC2.Models;
+
+namespace POC2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserDetailsController : ControllerBase
+    {
+        private readonly UserDataContext _context;
+
+        public UserDetailsController(UserDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UserDetails
+        [HttpGet("Details")]
+        public async Task<ActionResult<IEnumerable<UserDetail>>> GetUserDetails()
+        {
+            return await _context.UserDetails.ToListAsync();
+        }
+
+        // GET: api/UserDetails/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDetail>> GetUserDetail(int id)
+        {
+            var userDetail = await _context.UserDetails.FindAsync(id);
+
+            if (userDetail == null)
+            {
+                return NotFound();
+            }
+
+            return userDetail;
+        }
+
+       
+        // POST: api/UserDetails
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<UserDetail>> PostUserDetail(UserDetail userDetail)
+        {
+            _context.UserDetails.Add(userDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserDetail", new { id = userDetail.UserId }, userDetail);
+        }
+
+       
+
+        private bool UserDetailExists(int id)
+        {
+            return _context.UserDetails.Any(e => e.UserId == id);
+        }
+    }
+}
---
+++ POC2/Migrations/20220324090118_init.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POC2.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserDetails",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    LastName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Username = table.Column<string>(type: "varchar(80)", unicode: false, maxLength: 80, nullable: true),
+                    Email = table.Column<string>(type: "varchar(150)", unicode: false, maxLength: 150, nullable: true),
+                    Password = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDetails", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserDetails");
+        }
+    }
+}
